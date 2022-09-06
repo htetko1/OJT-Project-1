6bfb179891b7c1d94687f9a7d8005a896af77101
htetko <ko5602538@gmail.com>
2022-09-06 13:35:37 +0630

    one

---
+++ app/app.js
@@ -0,0 +1,12 @@
+// let screenHeight = $(window).height();
+//
+// $(window).scroll(function () {
+//     let current = $(this).scrollTop();
+//     // console.log(screenHeight);
+//     if (current > screenHeight - 100) {
+//         $(".site-nav").addClass("site-nav-scroll");
+//     } else {
+//         $(".site-nav").removeClass("site-nav-scroll");
+//         // setActive("home");
+//     }
+// });
